b232b871fa890273f72b5125b673d16d69a1500b
Elena <echaconv@alumnos.unex.es>
2023-05-27 15:20:22 +0200

    Etapa

--- Etapa.java
+++ Etapa.java
@@ -33,4 +33,54 @@ public class Etapa
     {
         this.nombre = nombre;
     }
+     /**
+     * Asigna la dificultad
+     * @param dificultad Double que especifica la dificultad de la etapa
+     */
+    public void setDificultad (double dificultad){
+        this.dificultad = dificultad;
+    }
+    
+    /**
+     * Asigna la distancia
+     * @param Int distancia Int que especifica la distancia de la etapa
+     */
+    public void setDistancia (int distancia){
+        this.distancia = distancia;
+    }
+    
+    //MÉTODOS DE ACCESO (get)
+    /**
+     * Devuelve el nombre
+     * @return String con el nombre de la etapa
+     */
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+    /**
+     * Devuelve la dificultad
+     * @return Double con la dificultad del recorrido
+     */
+    public double getDificultad (){
+        return dificultad;
+    }
+    
+    /**
+     * Devuelve la distancia
+     * @return Int con la distancia recorrida en la etapa
+     */
+    public int getDistancia () {
+        return distancia;
+    }
+    
+    /**
+     * Muestra las caracteristicas de etapa
+     * @return String que muestra las caracteristicas de la etapa
+     */
+    public String toString(){
+        return "<etapa: " + nombre + "> <dificultad:" + dificultad + "> <distancia: " + distancia + ")>";
+    }
+
 }
